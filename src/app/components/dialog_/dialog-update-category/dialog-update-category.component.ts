1d50e09b37dc0dabc98694e52d2fe216b58c4869
Briand <briandthibaud99@gmail.com>
2021-07-12 17:03:45 +0200

    implement dialog-update-category

--- src/app/components/dialog_/dialog-update-category/dialog-update-category.component.ts
+++ src/app/components/dialog_/dialog-update-category/dialog-update-category.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {CategoryService} from "../../../services/category/category.service";
+import {Category} from "../../../shared/models/category.model";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-dialog-update-category',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogUpdateCategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<DialogUpdateCategoryComponent>,
+              public _categoryService: CategoryService,
+              @Inject(MAT_DIALOG_DATA) public data: { category: Category}) { }
 
   ngOnInit(): void {
+
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+
+  onClickSubmit() {
+    this._categoryService.updateCategory(this.data.category).subscribe({
+      next: async () => {
+        this.dialogRef.close()
+      },
+      error: err => {
+        if (!environment.production) {
+          console.error('Error: ', err);
+        }
+      }
+    });
+  }
 }
