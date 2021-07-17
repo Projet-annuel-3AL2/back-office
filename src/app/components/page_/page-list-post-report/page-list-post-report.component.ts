20a8bb396945a59dcddc6912d4c2be82c490fcd8
Briand <briandthibaud99@gmail.com>
2021-07-17 19:52:19 +0200

    Implement component .ts

--- src/app/components/page_/page-list-post-report/page-list-post-report.component.ts
+++ src/app/components/page_/page-list-post-report/page-list-post-report.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {Report} from "../../../shared/models/report.model";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {ReportService} from "../../../services/report/report.service";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-page-list-post-report',
@@ -7,9 +13,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageListPostReportComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['userReporter', 'text', 'reportedPost','nbReport', 'createdAt', 'id'];
+  dataSource: MatTableDataSource<Report>
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit(): void {
+  constructor(public _reportService: ReportService) { }
+
+  async ngOnInit(): Promise<void> {
+    await this.updateData();
+  }
+
+  private async updateData() {
+    this._reportService.getPostReports().subscribe({
+      error: err => {
+        if (!environment.production) {
+          console.error('Error: ', err);
+        }
+      }
+    });
+    await this._reportService.reports.subscribe(reports => {
+      this.dataSource = new MatTableDataSource(reports);
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.sort !== null?this.dataSource.sort = this.sort: null;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteReport(reportId: string){
+    this._reportService.removeReport(reportId).subscribe({
+      error: err => {
+        if (!environment.production) {
+          console.error('Error: ', err);
+        }
+      }
+    });
   }
 
 }
