08c9e984f3061a1aa3c3079e526c4a99227af772
Briand <briandthibaud99@gmail.com>
2021-07-17 19:20:43 +0200

    Implement ReportRoute

--- src/app/shared/models/report.model.ts
+++ src/app/shared/models/report.model.ts
@@ -2,6 +2,7 @@ import {User} from "./user.model";
 import {Group} from "./group.model";
 import {Post} from "./post.model";
 import {Organisation} from "./organisation.model";
+import {Event} from "./event.model";
 
 export class Report {
   text: string;
@@ -11,4 +12,5 @@ export class Report {
   reportedPost?: Post;
   reportedOrganisation?: Organisation;
   reportedEvent?: Event;
+  nbReport: number;
 }
